8a9431b93e5828042b395a943633faece4e81661
NotEasyOk <masterplayz755@gmail.com>
2025-12-20 18:36:21 +0530

    Create TotemListener.java

---
+++ src/main/java/me/tokensmp/core/TotemListener.java
@@ -0,0 +1,18 @@
+package me.tokensmp.core.life;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityResurrectEvent;
+import org.bukkit.entity.Player;
+
+public class TotemListener implements Listener {
+
+    @EventHandler
+    public void onTotem(EntityResurrectEvent event) {
+        if (!(event.getEntity() instanceof Player player)) return;
+
+        if (LifeShieldManager.hasShield(player)) {
+            LifeShieldManager.consumeTotem(player);
+        }
+    }
+}
