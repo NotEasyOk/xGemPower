a87b40d8cd11f8f66ea0da78521167efd35528df
NotEasyOk <masterplayz755@gmail.com>
2025-12-20 11:34:05 +0530

    Create TokenSmpCore.java

---
+++ src/main/java/me/tokensmp/core/TokenSmpCore.java
@@ -0,0 +1,41 @@
+package me.tokensmp.core;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import me.tokensmp.core.life.LifeManager;
+import me.tokensmp.core.life.LifeListener;
+import me.tokensmp.core.life.LifeCommand;
+import me.tokensmp.core.token.TokenManager;
+import me.tokensmp.core.token.TokenListener;
+
+public class TokenSmpCore extends JavaPlugin {
+
+    private static TokenSmpCore instance;
+    private LifeManager lifeManager;
+    private TokenManager tokenManager;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+
+        saveDefaultConfig();
+
+        lifeManager = new LifeManager(this);
+        tokenManager = new TokenManager(this);
+
+        getServer().getPluginManager().registerEvents(new LifeListener(lifeManager), this);
+        getServer().getPluginManager().registerEvents(new TokenListener(tokenManager), this);
+
+        getCommand("tokens").setExecutor(new LifeCommand(lifeManager));
+
+        getLogger().info("TokenSMP-Core ENABLED");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("TokenSMP-Core DISABLED");
+    }
+
+    public static TokenSmpCore getInstance() {
+        return instance;
+    }
+  }
