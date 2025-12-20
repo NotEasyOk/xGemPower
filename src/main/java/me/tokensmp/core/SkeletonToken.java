3c2fa038f39cc8b4a02d25c8a9a0d93a2728f374
NotEasyOk <masterplayz755@gmail.com>
2025-12-20 12:41:50 +0530

    Create SkeletonToken.java

---
+++ src/main/java/me/tokensmp/core/SkeletonToken.java
@@ -0,0 +1,34 @@
+package me.tokensmp.core.token.abilities;
+
+import me.tokensmp.core.util.CooldownUtil;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Arrow;
+import org.bukkit.entity.Player;
+
+public class SkeletonToken {
+
+    private static final int COOLDOWN = 60;
+
+    public static void useAbility(Player player) {
+
+        if (CooldownUtil.isOnCooldown("skeleton_rain", player.getUniqueId(), COOLDOWN)) {
+            player.sendMessage("§cAbility on cooldown!");
+            return;
+        }
+
+        Location loc = player.getLocation();
+
+        for (int i = 0; i < 20; i++) {
+            loc.getWorld().spawnArrow(
+                    loc.clone().add(Math.random()*6-3, 10, Math.random()*6-3),
+                    loc.getDirection(),
+                    1.5f,
+                    12
+            );
+        }
+
+        player.sendMessage("§7Skeleton Arrow Rain!");
+        CooldownUtil.setCooldown("skeleton_rain", player.getUniqueId());
+    }
+          }
