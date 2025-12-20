4a071eaa67a05f45564260179c2925065b3a320c
NotEasyOk <masterplayz755@gmail.com>
2025-12-20 15:54:07 +0530

    Create CreeperToken.java

---
+++ src/main/java/me/tokensmp/core/CreeperToken.java
@@ -0,0 +1,27 @@
+package me.tokensmp.core.token.abilities;
+
+import me.tokensmp.core.util.CooldownUtil;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.TNTPrimed;
+
+public class CreeperToken {
+
+    private static final int COOLDOWN = 60;
+
+    public static void useAbility(Player player) {
+
+        if (CooldownUtil.isOnCooldown("creeper_orbit", player.getUniqueId(), COOLDOWN)) {
+            player.sendMessage("§cAbility on cooldown!");
+            return;
+        }
+
+        Location loc = player.getLocation().add(0, 15, 0);
+        TNTPrimed tnt = loc.getWorld().spawn(loc, TNTPrimed.class);
+        tnt.setFuseTicks(40);
+        tnt.setYield(6F);
+
+        player.sendMessage("§aCreeper Orbital Strike!");
+        CooldownUtil.setCooldown("creeper_orbit", player.getUniqueId());
+    }
+            }
