dcc62f723ff5f8ad7c8a3edc29322617f931e74a
NotEasyOk <masterplayz755@gmail.com>
2025-12-20 15:59:36 +0530

    Create DolphinToken.java

---
+++ src/main/java/me/tokensmp/core/DolphinToken.java
@@ -0,0 +1,25 @@
+package me.tokensmp.core.token.abilities;
+
+import me.tokensmp.core.util.CooldownUtil;
+import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+public class DolphinToken {
+
+    private static final int COOLDOWN = 60;
+
+    public static void useAbility(Player player) {
+
+        if (CooldownUtil.isOnCooldown("dolphin_swim", player.getUniqueId(), COOLDOWN)) {
+            player.sendMessage("§cAbility on cooldown!");
+            return;
+        }
+
+        player.addPotionEffect(new PotionEffect(PotionEffectType.DOLPHINS_GRACE, 300, 1));
+        player.addPotionEffect(new PotionEffect(PotionEffectType.WATER_BREATHING, 300, 0));
+
+        player.sendMessage("§3Dolphin Speed!");
+        CooldownUtil.setCooldown("dolphin_swim", player.getUniqueId());
+    }
+          }
