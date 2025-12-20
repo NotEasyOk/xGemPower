a6ae907fb5763f37f7dfb526bb247718fd91305b
NotEasyOk <masterplayz755@gmail.com>
2025-12-20 15:56:16 +0530

    Create WitherToken.java

---
+++ src/main/java/me/tokensmp/core/WitherToken.java
@@ -0,0 +1,25 @@
+package me.tokensmp.core.token.abilities;
+
+import me.tokensmp.core.util.CooldownUtil;
+import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+public class WitherToken {
+
+    private static final int COOLDOWN = 60;
+
+    public static void useAbility(Player player) {
+
+        if (CooldownUtil.isOnCooldown("wither_aura", player.getUniqueId(), COOLDOWN)) {
+            player.sendMessage("§cAbility on cooldown!");
+            return;
+        }
+
+        player.addPotionEffect(new PotionEffect(PotionEffectType.WITHER, 200, 1));
+        player.addPotionEffect(new PotionEffect(PotionEffectType.ABSORPTION, 200, 2));
+
+        player.sendMessage("§8Wither Aura Activated!");
+        CooldownUtil.setCooldown("wither_aura", player.getUniqueId());
+    }
+}
