a695496b5812e449e5e089d3035d4d046bd71de8
NotEasyOk <masterplayz755@gmail.com>
2025-12-21 15:28:06 +0530

    Update SkeletonToken.java

--- src/main/java/me/tokensmp/core/SkeletonToken.java
+++ src/main/java/me/tokensmp/core/SkeletonToken.java
@@ -1,6 +1,6 @@
 package me.tokensmp.core.token.abilities;
 
-import me.tokensmp.core.util.CooldownUtil;
+import me.tokensmp.core.token.CooldownManager;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Arrow;
