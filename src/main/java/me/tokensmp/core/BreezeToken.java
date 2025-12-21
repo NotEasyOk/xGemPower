741fe9a5158f0fdba8c7ddc365d44e5c364e6315
NotEasyOk <masterplayz755@gmail.com>
2025-12-21 15:24:13 +0530

    Update BreezeToken.java

--- src/main/java/me/tokensmp/core/BreezeToken.java
+++ src/main/java/me/tokensmp/core/BreezeToken.java
@@ -1,6 +1,6 @@
 package me.tokensmp.core.token.abilities;
 
-import me.tokensmp.core.util.CooldownUtil;
+import me.tokensmp.core.token.CooldownManager;
 import org.bukkit.entity.Player;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
