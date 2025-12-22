6bc9279b9b7e715d40c4d3f444570ecfbe90d0c5
NotEasyOk <masterplayz755@gmail.com>
2025-12-22 10:29:05 +0530

    Update TotemListener.java

--- src/main/java/me/tokensmp/core/TotemListener.java
+++ src/main/java/me/tokensmp/core/TotemListener.java
@@ -1,12 +1,19 @@
 package me.tokensmp.core.life;
 
+import me.tokensmp.core.life;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityResurrectEvent;
 import org.bukkit.entity.Player;
 
 public class TotemListener implements Listener {
-
+    
+    private final TokenSmpCore plugin;
+    
+    public TotemListener(TokenSmpCore plugin) {
+        this.plugin = plugin;
+    }
+    
     @EventHandler
     public void onTotem(EntityResurrectEvent event) {
         if (!(event.getEntity() instanceof Player player)) return;
