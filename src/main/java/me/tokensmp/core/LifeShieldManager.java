bfc6a4e7f9954b35e03e2e8c0d97aa945b812af1
NotEasyOk <masterplayz755@gmail.com>
2025-12-22 08:57:54 +0530

    Update LifeShieldManager.java

--- src/main/java/me/tokensmp/core/LifeShieldManager.java
+++ src/main/java/me/tokensmp/core/LifeShieldManager.java
@@ -8,25 +8,35 @@ import java.util.UUID;
 
 public class LifeShieldManager {
 
-    private static final Map<UUID, Integer> activeTotems = new HashMap<>();
+    private final LifeManager lifeManager;
+    private final Map<UUID, Integer> activeTotems = new HashMap<>();
 
-    public static void activate(Player player) {
+    public LifeShieldManager(LifeManager lifeManager) {
+        this.lifeManager = lifeManager;
+    }
+
+    public void activate(Player player) {
         activeTotems.put(player.getUniqueId(), 5);
     }
 
-    public static boolean hasShield(Player player) {
+    public boolean hasShield(Player player) {
         return activeTotems.containsKey(player.getUniqueId());
     }
 
-    public static void consumeTotem(Player player) {
-        int left = activeTotems.get(player.getUniqueId()) - 1;
+    public void consumeTotem(Player player) {
+        UUID uuid = player.getUniqueId();
+
+        if (!activeTotems.containsKey(uuid)) return;
+
+        int left = activeTotems.get(uuid) - 1;
 
         if (left <= 0) {
-            activeTotems.remove(player.getUniqueId());
+            activeTotems.remove(uuid);
             lifeManager.addLife(player, 1);
             player.sendMessage("§aLife returned automatically!");
         } else {
-            activeTotems.put(player.getUniqueId(), left);
+            activeTotems.put(uuid, left);
         }
     }
 }
+
