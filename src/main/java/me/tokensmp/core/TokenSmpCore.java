2eef8185bbb52b2188f83516f889394b4fcfe50a
NotEasyOk <masterplayz755@gmail.com>
2025-12-23 09:21:20 +0530

    Update TokenSmpCore.java

--- src/main/java/me/tokensmp/core/TokenSmpCore.java
+++ src/main/java/me/tokensmp/core/TokenSmpCore.java
@@ -1,19 +1,20 @@
 package me.tokensmp.core;
 
-import org.bukkit.plugin.java.JavaPlugin;
-import me.tokensmp.core.life.LifeManager;
 import me.tokensmp.core.life.LifeListener;
-import me.tokensmp.core.life.LifeCommand;
-import me.tokensmp.core.token.TokenManager;
-import me.tokensmp.core.token.TokenListener;
-import org.bukkit.NamespacedKey;
-import me.tokensmp.core.token.UnlockRecipeListener;
-import me.tokensmp.core.life.TotemListener;
+import me.tokensmp.core.life.LifeManager;
 import me.tokensmp.core.life.LifeShieldManager;
-
+import me.tokensmp.core.life.TotemListener;
+import me.tokensmp.core.token.TokenListener;
+import me.tokensmp.core.token.TokenManager;
+import org.bukkit.NamespacedKey;
+import org.bukkit.plugin.java.JavaPlugin;
+import me.tokensmp.core.life.LifeCommand
+ import me.tokensmp.core.token.UnlockRecipeListener
+    
 public class TokenSmpCore extends JavaPlugin {
 
     private static TokenSmpCore instance;
+
     private TokenManager tokenManager;
     private LifeManager lifeManager;
     private LifeShieldManager lifeShieldManager;
@@ -22,32 +23,52 @@ public class TokenSmpCore extends JavaPlugin {
     @Override
     public void onEnable() {
         instance = this;
+
+        // 🔥 THIS LINE FIXES FOLDER ISSUE
+        saveDefaultConfig(); // plugins/xGemPower/ create karega
+
+        // Optional safety
+        if (!getDataFolder().exists()) {
+            getDataFolder().mkdirs();
+        }
+
         tokenKey = new NamespacedKey(this, "token");
 
         tokenManager = new TokenManager(this);
         lifeManager = new LifeManager();
         lifeShieldManager = new LifeShieldManager(lifeManager);
-        
-        getServer().getPluginManager().registerEvents(
-            new TotemListener(this), this
-        );
-        
+
+        // Register listeners
         getServer().getPluginManager().registerEvents(
                 new TokenListener(tokenManager), this
         );
+
         getServer().getPluginManager().registerEvents(
                 new LifeListener(lifeManager), this
         );
+
+        getServer().getPluginManager().registerEvents(
+                new TotemListener(lifeShieldManager), this
+        );
+
+        getLogger().info("xGemPower enabled successfully!");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("xGemPower disabled!");
     }
 
     public static TokenSmpCore getInstance() {
         return instance;
     }
-    public LifeShieldManager getLifeShieldManager() {
-        return lifeShieldManager;
-    }
+
     public NamespacedKey getTokenKey() {
         return tokenKey;
+    }
+
+    public LifeShieldManager getLifeShieldManager() {
+        return lifeShieldManager;
     }
 }
 
